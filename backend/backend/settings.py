2137fefbaa02216a09d9fbee3094d8466da25c0f
dia77-creator <diyadinesh55@gmail.com>
2026-02-25 16:17:59 +0530

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,6 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://quickbite-order-management.vercel.app",
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
