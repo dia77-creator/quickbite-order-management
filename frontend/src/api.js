d5b48b4edee280593f3854af3298c33281e8000f
dia77-creator <diyadinesh55@gmail.com>
2026-02-25 15:57:41 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://quickbite-order-management.onrender.com/", // change after deployment
+  baseURL: "https://quickbite-order-management.onrender.com/api/menu/", // change after deployment
 });
 
 export default API;
