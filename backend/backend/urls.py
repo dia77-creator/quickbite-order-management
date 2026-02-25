31246e8b1832fc2a238b2c87817678a2dbffe669
dia77-creator <diyadinesh55@gmail.com>
2026-02-25 18:38:18 +0530

    Update urls.py

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,11 +18,17 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Backend is running successfully 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
-    path("api/", include("orders.urls")),
+    path('api/', include('orders.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
